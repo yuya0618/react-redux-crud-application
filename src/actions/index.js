e4a08a87f806611ed5ab7bd8db873b8127dd2601
yuya0618 <yuya.aki.0618@gmail.com>
2019-09-15 21:09:58 +0900

    events

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,10 +1,12 @@
-export const INCREMENT = 'INCREMENT'
-export const DECREMENT = 'DECREMENT'
+import axios from 'axios'
+import { dispatch } from '../../../../Library/Caches/typescript/3.5/node_modules/rxjs/internal/observable/pairs';
 
-export const increment = () =>({
-  type: 'INCREMENT'
-})
+export const READ_EVENTS = 'READ_EVENTS'
 
-export const decrement = () =>({
-  type: 'DECREMENT'
-})
+const ROOT_URL = 'https://udemy-utils.herokuapp.com/api/v1'
+const QUERYSTRING = '?token=token123'
+
+export const readEvents = () => async dispatch=>{
+  const response = await axios.get(`${ROOT_URL}/events${QUERYSTRING}`)
+  dispatch({ type: READ_EVENTS,response })
+}
