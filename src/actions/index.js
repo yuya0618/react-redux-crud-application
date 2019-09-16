bcd89f13490617237519eccff872a415e2ce8487
yuya0618 <yuya.aki.0618@gmail.com>
2019-09-16 13:54:51 +0900

    delete_event

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,6 +3,7 @@ import { dispatch } from '../../../../Library/Caches/typescript/3.5/node_modules
 
 export const READ_EVENTS = 'READ_EVENTS'
 export const CREATE_EVENT = 'CREATE_EVENT'
+export const DELETE_EVENT = 'DELETE_EVENT'
 
 
 const ROOT_URL = 'https://udemy-utils.herokuapp.com/api/v1'
@@ -16,4 +17,9 @@ export const readEvents = () => async dispatch=>{
 export const postEvent = values => async dispatch=>{
   const response = await axios.post(`${ROOT_URL}/events${QUERYSTRING}`, values)
   dispatch({ type: CREATE_EVENT,response })
+}
+
+export const deleteEvent = id => async dispatch=>{
+  await axios.delete(`${ROOT_URL}/events/${id}${QUERYSTRING}`)
+  dispatch({ type: DELETE_EVENT,id })
 }
--- src/reducers/events.js
+++ src/reducers/events.js
@@ -1,10 +1,16 @@
 import _ from 'lodash'
-import { READ_EVENTS } from '../actions'
+import {
+  READ_EVENTS,
+  DELETE_EVENT,
+} from '../actions'
 
 export default (events = {},action) => {
   switch(action.type) {
     case READ_EVENTS:
       return _.mapKeys(action.response.data, 'id')
+    case DELETE_EVENT:
+      delete events[action.id]
+      return { ...events }
     default:
       return events
   }
