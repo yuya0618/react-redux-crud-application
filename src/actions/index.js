d965d383b6794db9ee5d37344f6d01ccbe4ea636
yuya0618 <yuya.aki.0618@gmail.com>
2019-09-16 11:37:01 +0900

    new.events

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,6 +2,8 @@ import axios from 'axios'
 import { dispatch } from '../../../../Library/Caches/typescript/3.5/node_modules/rxjs/internal/observable/pairs';
 
 export const READ_EVENTS = 'READ_EVENTS'
+export const CREATE_EVENT = 'CREATE_EVENT'
+
 
 const ROOT_URL = 'https://udemy-utils.herokuapp.com/api/v1'
 const QUERYSTRING = '?token=token123'
@@ -9,4 +11,9 @@ const QUERYSTRING = '?token=token123'
 export const readEvents = () => async dispatch=>{
   const response = await axios.get(`${ROOT_URL}/events${QUERYSTRING}`)
   dispatch({ type: READ_EVENTS,response })
+}
+
+export const postEvent = values => async dispatch=>{
+  const response = await axios.post(`${ROOT_URL}/events${QUERYSTRING}`, values)
+  dispatch({ type: CREATE_EVENT,response })
 }
