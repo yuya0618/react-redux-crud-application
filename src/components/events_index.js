77fd053610478c0aaf01ef120ebe361007deb5b7
yuya0618 <yuya.aki.0618@gmail.com>
2019-09-16 15:10:04 +0900

    markup top

--- src/components/events_index.js
+++ src/components/events_index.js
@@ -2,6 +2,16 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux'
 import _ from 'lodash'
 import { Link } from 'react-router-dom'
+import {
+  Table,
+  TableBody,
+  TableHeader,
+  TableHeaderColumn,
+  TableRow,
+  TableRowColumn
+} from 'material-ui/Table'
+import FloatingActionButton from 'material-ui/FloatingActionButton'
+import ContentAdd from 'material-ui/svg-icons/content/add'
 
 
 import { readEvents } from '../actions'
@@ -28,25 +38,32 @@ class EventsIndex extends Component{
 
 
   render(){
-    const props = this.props
-
+    const style={
+      position:"fixed",
+      right:12,
+      bottom:12
+    }
     return(
       <React.Fragment>
-        <table>
-          <thead>
-            <tr>
-              <th>ID</th>
-              <th>Title</th>
-              <th>Body</th>
-            </tr>
-          </thead>
-  
-          <tbody>
+        <FloatingActionButton style={style} conrainerElement={<Link to="/events/new"/>}>
+          <ContentAdd/>
+        </FloatingActionButton>
+        <Table>
+          <TableHeader
+            displaySelectAll={false}
+            adjustForCheckbox={false}
+          >
+            <TableRow>
+              <TableHeaderColumn>ID</TableHeaderColumn>
+              <TableHeaderColumn>Title</TableHeaderColumn>
+              <TableHeaderColumn>Body</TableHeaderColumn>
+            </TableRow>
+          </TableHeader>
+
+          <TableBody displayRowCheckbox={false}>
             {this.renderEvents()}
-          </tbody>
-        </table>
-  
-        <Link to="/events/new">New Event</Link>
+          </TableBody>
+        </Table>
 
       </React.Fragment>
     )
--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import { Provider } from 'react-redux'
 import thunk from 'redux-thunk'
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import { composeWithDevTools } from 'redux-devtools-extension'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
 import './index.css';
 import reducer from './reducers'
@@ -16,19 +17,20 @@ import EventsShow from './components/events_show';
 
 const enhancer = process.env.NODE_ENV === 'development' ?
   composeWithDevTools(applyMiddleware(thunk)) : applyMiddleware(thunk)
-
 const store = createStore(reducer, enhancer)
 
 ReactDOM.render(
-  <Provider store={store}>
-    <BrowserRouter>
-      <Switch>
-        <Route path="/events/new" component={EventsNew}/>
-        <Route path="/events/:id" component={EventsShow}/>
-        <Route exact path="/" component={EventsIndex}/>
-        <Route exact path="/events" component={EventsIndex}/>
-      </Switch>
-    </BrowserRouter>
-  </Provider>,
+  <MuiThemeProvider>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Switch>
+          <Route path="/events/new" component={EventsNew}/>
+          <Route path="/events/:id" component={EventsShow}/>
+          <Route exact path="/" component={EventsIndex}/>
+          <Route exact path="/events" component={EventsIndex}/>
+        </Switch>
+      </BrowserRouter>
+    </Provider>
+  </MuiThemeProvider>,
   document.getElementById('root')
 );
